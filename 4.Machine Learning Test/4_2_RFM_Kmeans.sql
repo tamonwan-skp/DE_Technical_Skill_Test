14b5b9be968f420279efa21861e4a766dd77cd0c
Bow Tamonwan <tamonwan.sukumpee@gmail.com>
2025-03-06 13:01:56 +0700

    Add topic number

--- 4.Machine Learning Test/4_2_RFM_Kmeans.sql
+++ 4.Machine Learning Test/4_2_RFM_Kmeans.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE MODEL `my-etl-project-452702.etl_dataset.rfm_kmeans`
 OPTIONS(
     model_type='kmeans',
-    num_clusters=4 
+    num_clusters = 4 
 ) AS
 SELECT 
     recency, frequency, monetary
